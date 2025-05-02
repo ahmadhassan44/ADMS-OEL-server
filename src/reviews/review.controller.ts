27f38f5141a30bc477e4439a51eadccefd9347b5
Ahmad Hassan <146438862+ahmadhassan44@users.noreply.github.com>
2025-05-02 12:49:21 +0500

    feat/add controller endpoint and service function

--- src/reviews/review.controller.ts
+++ src/reviews/review.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { ReviewService } from './review.service';
+import { ReviewDto } from './dto/review.dto';
 
 @Controller()
-export class ReviewController {}
+export class ReviewController {
+  constructor(private readonly reviewService: ReviewService) {}
+  @Post('add-review')
+  addReview(@Body() reviewDto: ReviewDto) {
+    return this.reviewService.addReview(reviewDto);
+  }
+}
--- src/reviews/review.service.ts
+++ src/reviews/review.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Mode } from 'fs';
+import { Model } from 'mongoose';
+import { Review } from 'src/schema/review.schema';
+import { ReviewDto } from './dto/review.dto';
 
 @Injectable()
-export class ReviewService {}
+export class ReviewService {
+  constructor(@InjectModel(Review.name) private reviewModel: Model<Review>) {}
+
+  async addReview(reviewDto: ReviewDto) {
+    return 'hello';
+  }
+}
