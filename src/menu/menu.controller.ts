f23f619b06ed352a4236a40b2683e35e1a69c66d
Ahmad Hassan <146438862+ahmadhassan44@users.noreply.github.com>
2025-05-05 13:43:58 +0500

    feat/use uploads module as a dependency

--- src/menu/menu.controller.ts
+++ src/menu/menu.controller.ts
@@ -1,20 +1,64 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  UseInterceptors,
+  UploadedFiles,
+  ParseFilePipe,
+  MaxFileSizeValidator,
+  FileTypeValidator,
+  BadRequestException,
+} from '@nestjs/common';
 import { MenuService } from './menu.service';
 import { CreateMenuDto } from './dto/createMenu.dto';
+import { UploadsService } from 'src/uploads/uploads.service';
+import { FilesInterceptor } from '@nestjs/platform-express';
 
 @Controller('menu')
 export class MenuController {
-  constructor(private readonly menuService: MenuService) {}
+  constructor(
+    private readonly menuService: MenuService,
+    private readonly uploadsService: UploadsService,
+  ) {}
 
   @Post('restaurant/:id')
+  @UseInterceptors(FilesInterceptor('items', 10))
   async createMenuForRestaurant(
     @Param('id') restaurantId: string,
-    @Body() createMenuDto: CreateMenuDto,
+    @Body() body: any,
+    @UploadedFiles(
+      new ParseFilePipe({
+        validators: [
+          new MaxFileSizeValidator({ maxSize: 5 * 1024 * 1024 }),
+          new FileTypeValidator({ fileType: '.(png|jpeg|jpg|webp)' }),
+        ],
+        fileIsRequired: false,
+      }),
+    )
+    files: Array<Express.Multer.File>,
   ) {
-    return this.menuService.createMenuForRestaurant(
-      restaurantId,
-      createMenuDto,
-    );
+    let menuItems = body.menuItems;
+    try {
+      if (typeof menuItems === 'string') {
+        menuItems = JSON.parse(menuItems);
+      }
+
+      const createMenuDto: CreateMenuDto = {
+        menuItems: menuItems,
+      };
+
+      const fileUrls = await this.uploadsService.uploadMultipleImages(files);
+      return this.menuService.createMenuForRestaurant(
+        restaurantId,
+        createMenuDto,
+      );
+    } catch (error) {
+      throw new BadRequestException(
+        'Failed to parse menuItems: ' + error.message,
+      );
+    }
   }
 
   @Get('restaurant/:id')
--- src/menu/menu.module.ts
+++ src/menu/menu.module.ts
@@ -3,12 +3,14 @@ import { MenuService } from './menu.service';
 import { MenuController } from './menu.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Restaurant, RestaurantSchema } from 'src/schema/restaurant.schema';
+import { UploadsModule } from 'src/uploads/uploads.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: Restaurant.name, schema: RestaurantSchema },
     ]),
+    UploadsModule,
   ],
   controllers: [MenuController],
   providers: [MenuService],
